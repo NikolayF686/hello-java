1fac8a5fbae4473fe1a97b2378966076f682ce44
Nikolay Fedorov <nikolaff686@gmail.com>
2021-12-11 00:55:02 +0400

    Hello add

--- src/com/javalessons1/Son.java
+++ src/com/javalessons1/Son.java
@@ -3,5 +3,6 @@ package com.javalessons1;
 public class Son {
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
+        System.out.println("Hello");
     }
 }
