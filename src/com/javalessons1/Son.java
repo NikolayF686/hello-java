c95dc465bde359262601c398c1627ea6b34769ad
Nikolay Fedorov <nikolaff686@gmail.com>
2021-12-11 01:05:45 +0400

    change Son - ++ int

--- src/com/javalessons1/Son.java
+++ src/com/javalessons1/Son.java
@@ -3,6 +3,6 @@ package com.javalessons1;
 public class Son {
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
-        System.out.println("Hello");
+        int i = 1;
     }
 }
